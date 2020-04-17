f0b40afdc3a488c721fffa40f885d85bb568f3d3
tangtang <1987567948@qq.com>
2020-04-17 11:33:33 +0800

    新增：PDF-vue插件，增加样例文件

---
+++ simple-vue-pdf/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Pdfpage >这是PDF文件链接</Pdfpage>
+    <MorePage>更多页面</MorePage>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Pdfpage from './components/pdfpage'
+import MorePage from './components/MorePage'
+
+export default {
+  name: 'App',
+  components: {
+    Pdfpage,
+    MorePage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ simple-vue-pdf/src/components/pdfpage.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		{{currentPage}} / {{pageCount}}
+		<pdf
+			src="./pdfFiles/fugong.pdf"
+			@num-pages="pageCount = $event"
+			@page-loaded="currentPage = $event"
+		></pdf>
+	</div>
+</template>
+
+<script>
+
+import pdf from 'vue-pdf'
+
+export default {
+	components: {
+		pdf
+	},
+	data() {
+		return {
+			currentPage: 0,
+			pageCount: 0,
+		}
+	}
+}
+
+</script>
